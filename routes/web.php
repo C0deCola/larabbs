a2114f059a5f89ba09808acf81b4e38f20e0d29a
onesailma <codecola@126.com>
2020-08-14 07:37:51 +0000

    找回密码成功消息提示

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'PagesController@root')->name('root');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
 
 Auth::routes(['verify' => true]);
 
