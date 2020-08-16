84749da987c3ca7b85fb3c3c196f02fa5ba895cf
onesailma <codecola@126.com>
2020-08-16 08:20:21 +0000

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 Auth::routes(['verify' => true]);
 
 //Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
