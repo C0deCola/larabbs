724c080853437bdc35ce32b19860c0fb8377ada9
onesailma <codecola@126.com>
2020-08-18 04:53:02 +0000

    翻译 URL 标示

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Topic;
+use App\Handlers\SlugTranslateHandler;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -12,5 +13,9 @@ class TopicObserver
     public function saving(Topic $topic)
     {
         $topic->excerpt = make_excerpt($topic->body);
+
+        if ( ! $topic->slug) {
+            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+        }
     }
 }
